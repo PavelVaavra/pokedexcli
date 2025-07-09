c74651d6104aa791a332429167c7401b7f2fe4c6
PavelVaavra <pavelvavra.vm@gmail.com>
2025-07-09 18:05:55 +0200

    pokecache internal package functionality finished

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -2,10 +2,13 @@ package pokecache
 
 import (
 	"time"
+	"sync"
+	"fmt"
 )
 
 type Cache struct {
 	Entries map[string]cacheEntry
+	Mux *sync.Mutex
 }
 
 type cacheEntry struct {
@@ -14,9 +17,12 @@ type cacheEntry struct {
 }
 
 func NewCache(duration time.Duration) Cache {
-	return Cache{
+	cache := Cache{
 		Entries: map[string]cacheEntry{},
+		Mux: &sync.Mutex{},
 	}
+	go cache.reapLoop(duration)
+	return cache
 }
 
 func (cache *Cache) Add(key string, val []byte) {
@@ -24,9 +30,34 @@ func (cache *Cache) Add(key string, val []byte) {
 		createdAt: time.Now(),
 		val: val,
 	}
+	cache.Mux.Lock()
+	fmt.Println("Adding: ", key)
 	cache.Entries[key] = entry
+	cache.Mux.Unlock()
 }
 
 func (cache *Cache) Get(key string) (val []byte, exists bool) {
-	return cache.Entries[key].val, true
+	cache.Mux.Lock()
+	entry, ok := cache.Entries[key]
+	cache.Mux.Unlock()
+	if ok {
+		fmt.Println("Getting: ", key)
+		return entry.val, true
+	}
+	return nil, false
+}
+
+func (cache *Cache) reapLoop(duration time.Duration) {
+	ticker := time.NewTicker(duration)
+	for {
+		<-ticker.C
+		cache.Mux.Lock()
+		for key, value := range cache.Entries {
+			if time.Since(value.createdAt) > duration {
+				fmt.Println("Deleting: ", key)
+				delete(cache.Entries, key)
+			}
+		}
+		cache.Mux.Unlock()
+	}
 }
